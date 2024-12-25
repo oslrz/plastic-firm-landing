0d471f0f40750be00ec7fdd0aaf43ff9cd90719a
Solarz Olexandr <57533910+oslrz@users.noreply.github.com>
2024-12-25 15:44:53 +0200

    fix: repo name

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   app: {
-    baseURL: '/your-repository-name/', 
+    baseURL: '/plastic-firm-landing/', 
   },
   css: ['@/assets/css/tailwind.css'],
   postcss: {
